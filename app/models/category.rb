87892289f698dbcd8b41f6d970e8ab7b9eadec43
Paulene Pallan <paulenepallan@gmail.com>
2024-03-11 20:05:36 +0800

    updated max category name to 50 characters

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,5 @@ class Category < ApplicationRecord
   belongs_to :user
   has_many :tasks, dependent: :destroy
 
-  validates :name, presence: true, length: { maximum: 20 }
+  validates :name, presence: true, length: { maximum: 50 }
 end
