889bbe612e65fdc5ddec991c7f1cd924be42e9f6
Paulene Pallan <paulenepallan@gmail.com>
2024-03-12 18:39:49 +0800

    refactor scopes for today and upcoming. today: simplified today scope by replacing a range comparison. upcoming: modified upcoming scope to exclude tasks due today and include those within the next 2 weeks. This ensures consistency in task categorization

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,10 +2,10 @@ class Task < ApplicationRecord
   belongs_to :category
   belongs_to :user
 
-  scope :today, -> { where("due_date >= ? AND due_date < ? AND completed = ?", Date.current, Date.current + 1, false).order(due_date: :asc) }
+  scope :today, -> { where(due_date: Date.current...(Date.current + 1), completed: false )}
   scope :overdue, -> { where("due_date < ? AND completed = ?", Date.current, false).order(due_date: :asc) }
   scope :completed, -> { where(completed: true) }
-  scope :upcoming, -> {where(due_date: Date.current..(Date.current + 2.weeks), completed: false).order(due_date: :asc)} # current date + 2 weeks
+  scope :upcoming, -> {where(due_date: (Date.current + 1)..(Date.current + 2.weeks), completed: false).order(due_date: :asc)}
 
   validates :name, presence: true
   validates :description, length: { maximum: 200 }
