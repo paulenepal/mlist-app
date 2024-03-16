50edd9130af3ef99e07c9f713ebee0ee899613d2
Paulene Pallan <paulenepallan@gmail.com>
2024-03-16 09:13:11 +0800

    fixed attribute assignment for task object, this will ensure that the description and completed attributes are directly assigned as values for task'

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -6,15 +6,15 @@ class TaskTest < ActiveSupport::TestCase
   # end
   test "should not save Task without name" do
     task = Task.new
-    description = 'Lorem ipsum..'
-    completed = false
+    task.description = 'Lorem ipsum..'
+    task.completed = false
     assert_not task.save, "Saved the Task without name"
   end
 
   test "should not save Task if description length > 200" do
     task = Task.new
-    description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-    completed = false
+    task.description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    task.completed = false
     assert_not task.save, "Saved the Task without name"
   end
 
